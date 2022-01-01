50e2d663f92fd61d341c29430e5ca05599acef23
OybekKayumov <oybek.kayumov@gmail.com>
2022-01-01 10:33:38 +0500

    add modal .html .css and .js

---
+++ scripts/modal.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i + 1) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+}
